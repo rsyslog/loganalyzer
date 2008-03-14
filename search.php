009cc63d1f497ef8b14a160afa079c6004e609d9
Andre Lorbach <alorbach@adiscon.com>
2008-03-14 15:53:55 +0100

    -splitted search from index site

---
+++ search.php
@@ -0,0 +1,54 @@
+<?php
+/*
+	*********************************************************************
+	* Copyright by Adiscon GmbH | 2008!									*
+	* -> www.phplogcon.org <-											*
+	*																	*
+	* Use this script at your own risk!									*
+	* -----------------------------------------------------------------	*
+	* Main Index File													*
+	*																	*
+	* -> Loads the main PhpLogCon Site									*
+	*																	*
+	* All directives are explained within this file						*
+	*********************************************************************
+*/
+
+// *** Default includes	and procedures *** //
+define('IN_PHPLOGCON', true);
+$gl_root_path = './';
+include($gl_root_path . 'include/functions_common.php');
+include($gl_root_path . 'include/functions_frontendhelpers.php');
+include($gl_root_path . 'include/functions_filters.php');
+
+InitPhpLogCon();
+InitSourceConfigs();
+InitFrontEndDefaults();	// Only in WebFrontEnd
+
+// Init Langauge first!
+IncludeLanguageFile( $gl_root_path . '/lang/' . $LANG . '/main.php' );
+
+// Helpers for frontend filtering!
+InitFilterHelpers();	
+// ***					*** //
+
+// --- CONTENT Vars
+// ---
+
+//if ( isset($content['myserver']) ) 
+//	$content['TITLE'] = "PhpLogCon :: Home :: Server '" . $content['myserver']['Name'] . "'";	// Title of the Page 
+//else
+	$content['TITLE'] = "PhpLogCon :: Search";
+// --- 
+
+// --- BEGIN Custom Code
+
+// --- 
+
+// --- Parsen and Output
+InitTemplateParser();
+$page -> parser($content, "search.html");
+$page -> output(); 
+// --- 
+
+?>
