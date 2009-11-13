a1e091a810f6f3f44fbcc9a25685f39e57f9d1ed
Andre Lorbach <alorbach@adiscon.com>
2009-11-13 10:12:17 +0100

    Added missing include file in maintenance.php

--- src/cron/maintenance.php
+++ src/cron/maintenance.php
@@ -37,6 +37,7 @@ $gl_root_path = './../';
 
 // Now include necessary include files!
 include_once($gl_root_path . 'include/functions_common.php');
+include_once($gl_root_path . 'include/functions_frontendhelpers.php');
 //include_once($gl_root_path . 'include/functions_debugoutput.php');
 
 // Set commandline mode for the script
