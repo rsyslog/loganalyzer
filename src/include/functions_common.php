8e1a98602562977268b537091ab89a18f0af8013
Andre Lorbach <alorbach@adiscon.com>
2012-09-10 14:51:13 +0200

    Added changelog for 3.4.5

--- src/include/functions_common.php
+++ src/include/functions_common.php
@@ -66,7 +66,7 @@ $LANG_EN = "en";	// Used for fallback
 $LANG = "en";		// Default language
 
 // Default Template vars
-$content['BUILDNUMBER'] = "3.4.4";
+$content['BUILDNUMBER'] = "3.4.5";
 $content['UPDATEURL'] = "http://loganalyzer.adiscon.com/files/version.txt";
 $content['TITLE'] = "Adiscon LogAnalyzer :: Release " . $content['BUILDNUMBER'];	// Default page title 
 $content['BASEPATH'] = $gl_root_path;
